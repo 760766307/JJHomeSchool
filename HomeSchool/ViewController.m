624a5eb647135cee5aab113a0bbe370decd1a621
760766307 <jiangedabaojian@qq.com>
2016-09-18 11:17:35 +0800

    杨剑
    第四版本

--- HomeSchool/ViewController.m
+++ HomeSchool/ViewController.m
@@ -34,6 +34,15 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
 }
 
 @end
