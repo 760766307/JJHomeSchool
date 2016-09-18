c86ab4adffb41646a609cbea635b4ebc6d49c36e
760766307 <jiangedabaojian@qq.com>
2016-09-18 11:46:14 +0800

    卧槽怎么什么也没了
    This reverts commit 172bf935d088e8e794ca17f094f25bd22e5dda85.
    
    # Conflicts:
    #	HomeSchool.xcodeproj/project.pbxproj
    #
    HomeSchool.xcodeproj/project.xcworkspace/xcuserdata/yangjian.xcuserdatad
    /UserInterfaceState.xcuserstate

--- HomeSchool/ViewController.m
+++ HomeSchool/ViewController.m
@@ -16,22 +16,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    NSLog(@"天啦噜");
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+    
+    
+    
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
